b8006accc8f9ac7c1a7ee3f99a18bb91833b17af
DharshanDinesh <75611213+DharshanDinesh@users.noreply.github.com>
2022-09-26 14:37:44 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+  const hex = {
+    1: {
+      x: 0,
+      y: 270,
+    },
+    2: {
+      x: 160,
+      y: 0,
+    },
+    3: {
+      y: 0,
+      x: 485,
+    },
+    4: {
+      y: 270,
+      x: 645,
+    },
+    5: {
+      y: 560,
+      x: 480,
+    },
+    6: {
+      y: 560,
+      x: 160,
+    },
+  };
+
+  function daa(a = hex, b = 1) {
+    let str = [];
+    const adsf = Object.values(a);
+    adsf.forEach((data) => {
+      str = [...str, data.x / b, data.y / b];
+    });
+    return str;
+  }
+
   return (
     <div>
       <svg
@@ -11,13 +47,12 @@ export default function App() {
         width="100%"
         height="100%"
       >
-        <polygon points="270,0 0,160 0,485 270,645 560,480 560,160"></polygon>
-        <polygon
-          points="270,0 135,80 0,485 270,645 560,480 560,160"
-          opacity="0.4"
-          fill="red"
-        ></polygon>
+        <Otha pt={daa(hex, 3)} />
       </svg>
     </div>
   );
+}
+
+function Otha({ pt }) {
+  return <polygon points={pt}></polygon>;
 }
